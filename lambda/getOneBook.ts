7dea34a4a7f94d5ba90f4dcf58d8b952dfd1949a
Tariq15994 <tariqnawaz15994@gmail.com>
2021-09-30 11:16:45 +0500

    Create getOneBook.ts
    this will return one book record

---
+++ lambda/getOneBook.ts
@@ -0,0 +1,34 @@
+import * as AWS from "aws-sdk";
+
+const TABLE_NAME = process.env.TABLE_NAME || "";
+const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
+
+const db = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async (event: any = {}): Promise<any> => {
+    const requestedItemId = event.path.Paramaters.id;
+
+    if (!requestedItemId) {
+        return {
+            status: 400,
+            body: "Error : You are missing the path parameter id",
+        };
+    }
+
+
+  const params = {
+    TableName: TABLE_NAME,
+    key : {
+        [PRIMARY_KEY]: requestedItemId,
+    },
+
+  };
+
+  try {
+    const response = await db.get(params).promise();
+    return { statusCode: 200, body: JSON.stringify(response.Item) };
+  } catch (err) {
+    console.log("DynamoDB error: ", err);
+    return { statusCode: 500, body: err };
+  }
+};
